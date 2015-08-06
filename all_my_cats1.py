638f89ef80b8b11d1c18e589c377f6ebde6ce858
Chad Bartel <chadbartel@users.noreply.github.com>
2015-08-06 07:51:54 -0700

    Delete all_my_cats1.py

--- all_my_cats1.py
+++
@@ -1,9 +0,0 @@
-cat_name1 = input('Enter the name of cat 1: ')
-cat_name2 = input('Enter the name of cat 2: ')
-cat_name3 = input('Enter the name of cat 3: ')
-cat_name4 = input('Enter the name of cat 4: ')
-cat_name5 = input('Enter the name of cat 5: ')
-cat_name6 = input('Enter the name of cat 6: ')
-print('The cat names are:')
-print(' '.join([cat_name1, cat_name2, cat_name3,
-                cat_name4, cat_name5, cat_name6]))
